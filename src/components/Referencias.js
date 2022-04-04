39e9949729e7046697f35529e59264ecfac774e7
dashdancing <omar.gbet@gmail.com>
2022-04-03 21:15:07 -0500

    Referencias MenuRef y refMenuBtn

--- src/components/Referencias.js
+++ src/components/Referencias.js
@@ -1,5 +1,20 @@
 import { useState, useEffect } from 'react';
 
 export default function Referencias(){
+    // let refMenu = createRef(),
     let refMenu = useRef(),
+        refMenuBtn = useRef();
+    
+        console.log(refMenu, refMenuBtn);
+    const handleToggleMenu = (e) => {
+        // const $menu = document.getElementById("menu");
+
+        // if (e.target.textContent === "Menú") {
+        //     e.target.textContent = "Cerrar";
+        //     $menu.style.display = "block";
+        // } else {
+        //     e.target.textContent = "Menú";
+        //     $menu.style.display = "none";
+        // }
+    } 
 }
