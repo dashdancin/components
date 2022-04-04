653fc0ab91f081194f8ba060727293df5518313a
dashdancing <omar.gbet@gmail.com>
2022-04-03 20:12:25 -0500

    ADD HooksPersonalizados useFetch

---
+++ src/components/HooksPersinalizados.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import { useFetch } from '../hooks/useFetch';
+
+export default function HooksPersonalizados() {
+    let url = "http://pokeapi.co/api/v2/pokemon/";
+    url = "http: jsonplaceholder.typicode.com/users";
+    console.log(useFetch());
+
+    let { data, isPending, error } = useFetch(url);
+
+    return (
+        <>
+            <h2>Hooks HooksPersonalizados</h2>
+            <h3>{JSON.stringify(isPending)}</h3>
+            <h3>
+                <mark>{JSON.stringify(error)}</mark>
+            </h3>
+            <pre style={{ whiteSpace: "pre-wrap"}}>
+                <code style={{ wordBreak: "break-word"}}>
+                    {JSON.stringify(data)}
+                </code>
+            </pre>
+        </>
+    )
+}
