404075aa1a3f5ea24e9f60c24a9ec991ef0c72c7
dashdancing <omar.gbet@gmail.com>
2022-04-04 21:33:50 -0500

    Nav + a

--- src/components/Referencias.js
+++ src/components/Referencias.js
@@ -30,7 +30,20 @@ export default function Referencias(){
     return (
         <>
         <h2>Referencias</h2>
-        <button id="menu-btn" ref={refMenuBtn} onClick={handleToggleMenu}></button>
+        <button id="menu-btn" ref={refMenuBtn} onClick={handleToggleMenu}>
+            Menú
+        </button>
+        <nav id="menu" ref={refMenu} style={{ display: "none" }}>
+            <a href='#'>Sección 1</a>
+            <br />
+            <a href='#'>Sección 2</a>
+            <br />
+            <a href='#'>Sección 3</a>
+            <br />
+            <a href='#'>Sección 4</a>
+            <br />
+            <a href='#'>Sección 5</a>
+        </nav>
         </>
     )
 }
