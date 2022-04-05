f77ecd144af9a9bf1fe11a17a2a15ad836409a6b
dashdancing <omar.gbet@gmail.com>
2022-04-04 22:58:55 -0500

    COMPLETE Form 3

--- src/components/Formularios.js
+++ src/components/Formularios.js
@@ -1,10 +1,114 @@
 import React, { useState } from 'react';
 
+// export default function Formularios() {
+//     const [nombre, setNombre] = useState ('');
+//     const [sabor, setSabor] = useState ('');
+//     const [lenguaje, SetLenguaje] = useState ('');
+//     const [terminos, setTerminos] = useState (false);
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         alert('El formulario se ha enviado');
+//     };
+
+//     return (
+//         <>
+//             <h2>Formularios</h2>
+//             <form onSubmit={handleSubmit}>
+//                 <label htmlFor='nombre'>Nombre:</label>
+//                 <input
+//                     type='text'
+//                     id='nombre'
+//                     name='nombre'
+//                     value={nombre}
+//                     onChange={(e) => setNombre(e.target.value)}
+//                 />
+//             <p>Elige tu sabor JS Favorito:</p>
+//                 <input
+//                     type='radio'
+//                     id='vanilla'
+//                     name='sabor'
+//                     value='vanilla'
+//                     onChange={(e) => setSabor(e.target.value)}
+//                     defaultChecked
+//                 />
+//                 <label htmlFor='vanilla'>Vanilla</label>
+//                 <input
+//                     type='radio'
+//                     id='react'
+//                     name='sabor'
+//                     value='react'
+//                     onChange={(e) => setSabor(e.target.value)}
+//                 />
+//                 <label htmlFor='react'>React</label>
+//                 <input 
+//                     type='radio'
+//                     id='angular'
+//                     name='sabor'
+//                     value='angular'
+//                     onChange={(e) => setSabor(e.target.value)}
+//                 />
+//                 <label htmlFor='angular'>Angular</label>
+//                 <input htmlFor='radio'
+//                     type='radio'
+//                     id='vue'
+//                     name='sabor'
+//                     value='vue'
+//                     onChange={(e) => setSabor(e.target.value)}
+//                 />
+//                 <label htmlFor='vue'>Vue</label>
+//                 <input
+//                     type='radio'
+//                     id='svelte'
+//                     name="sabor"
+//                     value='svelte'
+//                     onChange={(e) => setSabor(e.target.value)}
+//                 />
+//                 <label htmlFor='svelte'>Svelte</label>
+//                 <p>Elige tu lenguaje de programación favorito</p>
+//                 <select
+//                 name='lenguaje'
+//                 onChange={(e) => SetLenguaje(e.target.value)}
+//                 defaultValue=''
+//                 >
+//                 <option value=''>- - -</option>
+//                 <option value='js'>JavScript</option>
+//                 <option value='php'>PHP</option>
+//                 <option value='py'>Phyton</option>
+//                 <option value='go'>GO</option>
+//                 <option value='rb'>Ruby</option>
+//                 </select>
+//                 <br/>
+//                 <label htmlFor='terminos'>Acepto términos y condiciones</label>
+//                 <input
+//                 type='checkbox'
+//                 id='terminos'
+//                 name='terminos'
+//                 onChance={(e) => setTerminos(e.target.checked)}
+//                 />
+//                 <br/>
+//                 <input type='submit'/>
+//             </form>
+//         </>
+//     );
+// }
+
 export default function Formularios() {
-    const [nombre, setNombre] = useState ('');
-    const [sabor, setSabor] = useState ('');
-    const [lenguaje, SetLenguaje] = useState ('');
-    // const [terminos, setTerminos] = useState (false);
+    const [form, setForm] = useState({});
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+        const handleChecked = (e) => {
+            setForm({
+                ...form,
+                [e.target.name]: e.target.checkend,
+            });
+        };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,75 +124,20 @@ export default function Formularios() {
                     type='text'
                     id='nombre'
                     name='nombre'
-                    value={nombre}
-                    onChange={(e) => setNombre(e.target.value)}
-                />
-            <p>Elige tu sabor JS Favorito:</p>
+                    value={form.name}
+                    onChange={handleChange}
+                    />
+                <p>Elige tu Sabor JS Favorito</p>
                 <input
-                    type='radio'
-                    id='vanilla'
-                    name='sabor'
-                    value='vanilla'
-                    onChange={(e) => setSabor(e.target.value)}
+                    type="radio"
+                    id="vanilla"
+                    name="sabor"
+                    value="vanilla"
+                    onChange={handleChange}
                     defaultChecked
                 />
-                <label htmlFor='vanilla'>Vanilla</label>
-                <input
-                    type='radio'
-                    id='react'
-                    name='sabor'
-                    value='react'
-                    onChange={(e) => setSabor(e.target.value)}
-                />
-                <label htmlFor='react'>React</label>
-                <input 
-                    type='radio'
-                    id='angular'
-                    name='sabor'
-                    value='angular'
-                    onChange={(e) => setSabor(e.target.value)}
-                />
-                <label htmlFor='angular'>Angular</label>
-                <input htmlFor='radio'
-                    type='radio'
-                    id='vue'
-                    name='sabor'
-                    value='vue'
-                    onChange={(e) => setSabor(e.target.value)}
-                />
-                <label htmlFor='vue'>Vue</label>
-                <input
-                    type='radio'
-                    id='svelte'
-                    name="sabor"
-                    value='svelte'
-                    onChange={(e) => setSabor(e.target.value)}
-                />
-                <label htmlFor='svelte'>Svelte</label>
-                <p>Elige tu lenguaje de programación favorito</p>
-                <select
-                name='lenguaje'
-                onChange={(e) => setLenguaje(e.target.value)}
-                defaultValue=''
-                >
-                <option value=''>- - -</option>
-                <option value='js'>JavScript</option>
-                <option value='php'>PHP</option>
-                <option value='py'>Phyton</option>
-                <option value='go'>GO</option>
-                <option value='rb'>Ruby</option>
-                </select>
-                <br/>
-                <label htmlFor='terminos'>Acepto términos y condiciones</label>
-                <input
-                type='checkbox'
-                id='terminos'
-                name='terminos'
-                onChance={(e) => setTerminos(e.target.checked)}
-                />
-                <br/>
-                <input type='submit'/>
+                <label htmlFor=''></label>
             </form>
         </>
-    );
+    )
 }
