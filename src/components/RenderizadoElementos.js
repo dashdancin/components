8b4db853450bbd8a7816b5daddcec470dc826f47
dashdancing <omar.gbet@gmail.com>
2022-04-05 21:01:21 -0500

    Fix warning json Elemento Lista

--- src/components/RenderizadoElementos.js
+++ src/components/RenderizadoElementos.js
@@ -1,10 +1,20 @@
 import React, {Component} from "react";
+import data from '../helpers/data.json'
 
+function ElementoLista(props){
+    return(
+        <li>
+            <a href={props.el.web}>
+                {props.el.name}
+            </a>
+        </li>
+    );
+}
 export default class RenderizadodeElementos extends Component {
     constructor(props){
         super(props);
         this.state = {
-            season: ['Primavera','Verano','Otoño','Invierno'],
+            season: ['Primavera', 'Verano', 'Otoño', 'Invierno'],
         };
     }
     render(){
@@ -17,6 +27,12 @@ export default class RenderizadodeElementos extends Component {
                         <il key={index}>{el}</il>
                     ))}
                 </ol>
+                <h3>Frameworks Frontend JavaScript</h3>
+                <ul>
+                    {data.frameworks.map((el) => (
+                    <ElementoLista key={el.id} el={el}/>
+                    ))}
+                </ul>
             </div>
         );
     }
