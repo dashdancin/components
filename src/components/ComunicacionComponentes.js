dbef570a07b6fc86639dbb7bef4b137384072cae
dashdancing <omar.gbet@gmail.com>
2022-04-07 12:16:24 -0500

    Contador en Comunicacion hijos

--- src/components/ComunicacionComponentes.js
+++ src/components/ComunicacionComponentes.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
 
 export default class Padre extends Component {
+    state = {
+        contador: 0,
+    };
+    incrementarContador =(e) => {
+        this.setState({
+            contador:this.state.contador + 1
+        })
+    }
+
     render(){
         return(
             <>
