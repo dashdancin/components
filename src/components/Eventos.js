1c841e30e7a67edce5363fba27edbc8796c249a4
dashdancing <omar.gbet@gmail.com>
2022-04-06 23:05:17 -0500

    Fixed Eventos Contador botton + and botton -

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,17 +1,18 @@
 import React, {Component} from "react";
 
 export default class Eventos extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            contador: 0,
+        };
+        } 
+
     render(){
-        constructor(props) {
-            super(props);
-            this.state = {
-                contador: 0,
-            };
-        }
         return(
             <div>
                 <h2>Eventos en Componentes de Clase</h2>
-                <h3>{this.state-contador}</h3>
+                <h3>{this.state.contador}</h3>
                 <nav>
                     <button>+</button>
                     <button>-</button>
