6aebbf00b5d9ec5abbf07f97661fdfb971fc0bc1
dashdancing <omar.gbet@gmail.com>
2022-04-08 13:10:06 -0500

    Mensaje al DOM

--- src/components/CicloVida.js
+++ src/components/CicloVida.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
 
 export default class CicloVida extends Component {
+    constructor(props){
+        super(props);
+        console.log(0, "El componente se inicializa, aún NO está en el DOM")
+    }
     render(){
+        console.log(4, 
+            "El componente se redibuja por algún cambio en el DOM")
         return(
         <>
             <h2>Ciclo de Vida de los Componentes de Clase </h2>
