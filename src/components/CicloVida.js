9e379ae2d972ee8d137ecda07a1354c9526d79e3
dashdancing <omar.gbet@gmail.com>
2022-04-08 13:20:43 -0500

    Metodo tic tac

--- src/components/CicloVida.js
+++ src/components/CicloVida.js
@@ -7,9 +7,23 @@ export default class CicloVida extends Component {
     
         this.state = {
             hora:new Date().toLocaleTimeString()
-        }
-    
+        };
+
+        this.temporizador = null;
+            this.temporizador = setInterval(() => {
+                this.setState({
+                    hora: new Date().toLocaleDateString()
+                })
+            }, 1000);
+            }
     }
+
+    tictac = () => {}
+
+    iniciar = () => {};
+
+    detener = () => {};
+
     render(){
         console.log(4, 
             "El componente se redibuja por algún cambio en el DOM")
@@ -17,6 +31,8 @@ export default class CicloVida extends Component {
         <>
             <h2>Ciclo de Vida de los Componentes de Clase </h2>
             <h3>{this.state.hora}</h3>
+            <button onClick={}>iniciar</button>
+            <button onClick={}>detener</button>
         </>
         )
     }
