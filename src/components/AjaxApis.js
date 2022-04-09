c1f0d1f0548bd5d103b46fb5d0418376deffcf20
dashdancing <omar.gbet@gmail.com>
2022-04-09 16:17:02 -0500

    Peticion Ajax Pokemon

--- src/components/AjaxApis.js
+++ src/components/AjaxApis.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
+function Pokemon (props) {
+    return(
+        <figure>
+            <img src={props.avatar} alt={props.name}/>
+            <figcaption>{props.name}</figcaption>
+        </figure>
+    );
+}
 export default class AjaxApis extends Component {
+    state= {
+        pokemons:[],
+    };
+    
+    componentDidMount(){
+        let url = "https://pokeapi.co/api/v2/pokemon/";
+        fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+        })
+    }
     render(){
         return (
             <>
