44a9663a623a2d7c26e4f11989c4412d04852583
dashdancing <omar.gbet@gmail.com>
2022-04-09 19:31:21 -0500

    COMPLETE ejercicio Ajax con Pokemons

--- src/components/AjaxApis.js
+++ src/components/AjaxApis.js
@@ -4,13 +4,14 @@ function Pokemon (props) {
     return(
         <figure>
             <img src={props.avatar} alt={props.name}/>
-            <figcaptin>{props.name}</figcaptin>
+            <figcaption>{props.name}</figcaption>
         </figure>
     );
 }
+
 export default class AjaxApis extends Component {
     state= {
-        pokemons:[],
+        pokemons: [],
     };
     
     componentDidMount(){
@@ -29,17 +30,25 @@ export default class AjaxApis extends Component {
                         name: json.name,
                         avatar: json.sprites.front_default,
                     };
+
                     let pokemons = [...this.state.pokemons, pokemon];
+
                     this.setState({ pokemons });
                 });
             });
-        })
+        });
     }
     render(){
         return (
             <>
                 <h2>Peticiones Asincronas en Componentes de clase</h2>
-                {this.state.pokemons.map(el => <Pokemon key={el.id} name={el.name} avatar={el.avatar}/>)}
+                {this.state.pokemons.length === 0 ? (
+                    <h3>Cargando...</h3>
+                ) : (
+                    this.state.pokemons.map((el) => (
+                    <Pokemon key={el.id} name={el.name} avatar={el.avatar}/>
+                    ))
+                )}
             </>
         );
     }
