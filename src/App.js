5dad85bd0a26388272bf4103ee69d7b8cf22b80b
dashdancing <omar.gbet@gmail.com>
2022-05-02 15:15:56 -0500

    ADD propiedades

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
-import Componente from "./components/Componente";
+import Componente from './components/Componente';
+import Propiedades from './components/Propiedades'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <section>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,19 +19,22 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a>          
+        </section>
       <section>
-        <Componente msg="Hola soy un componente Funcional desde una prop" />
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+        <Componente msg="Hola soy un componente funcional desde una prop" />
+        <Propiedades
+        cadena="Esto no es una cadena de texto"
+        numero={19}
+        booleano={true}
+        arreglo={[1, 2, 3]}
+        objeto={{nombre: "Jon", correo: "jon.mircha@gmail.com"}}
+        function={(num) => num * num}
+        elementoReact={<i>Esto es un elemento React</i>}
+        componenteReact={<Componente msg="Soy un componente pasado como Prop"/>}
+        />
       </section>
+      </header>
     </div>
   );
 }
--- src/components/Componente.js
+++ src/components/Componente.js
@@ -1,9 +1,7 @@
 import React, {Component} from "react";
 
-class Componente extends Component {
-render(){
-    return <h2>{this.props.msg}</h2>
-}
+function Componente(props){
+return <h2>{props.msg}</h2>;
 }
 
 export default Componente;
