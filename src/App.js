f3eecdfde0bb0300b855f60c525206d8885e5907
dashdancing <omar.gbet@gmail.com>
2022-05-02 13:11:28 -0500

    ADD Componente

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Componente from "./components/Componente";
 import './App.css';
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <section>
+        <Componente/>
+      </section>
     </div>
   );
 }
