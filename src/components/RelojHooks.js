3fe2c7d1045bb2cccf924aa57d360ae702976770
dashdancing <omar.gbet@gmail.com>
2022-05-03 23:05:33 -0500

    Completado Reloj Hooks

--- src/components/RelojHooks.js
+++ src/components/RelojHooks.js
@@ -1,9 +1,36 @@
 import React, {useState, useEffect} from "react";
 
-export default function RelojHooks(){
-    return(
+function Reloj({ hora }) {
+    return <h3>{hora}</h3>;
+}
+
+export default function RelojHooks() {
+    const [hora, setHora] = useState(new Date().toLocaleTimeString());
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        let temporizador;
+
+        if(visible){
+            temporizador = setInterval(() => {
+                setHora(new Date().toLocaleTimeString());
+            }, 1000);
+        } else {
+            clearInterval(temporizador);
+        }
+    
+    return () => {
+        console.log("Fase de Desmonyaje");
+        clearInterval(temporizador);
+    };
+    }, [visible]);
+
+    return (
         <>
           <h2>Reloj con Hooks</h2>
+          {visible && <Reloj hora={hora}/>}
+          <button onClick={()=> setVisible(true)}>iniciar</button>
+          <button onClick={()=> setVisible(false)}>detener</button>
         </>
     );
 }
