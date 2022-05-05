3347c7337a66f37b6ff1ed7314de46611861567a
dashdancing <omar.gbet@gmail.com>
2022-05-04 19:08:03 -0500

    Ajax Hooks useEffect Pokemons API

--- src/components/AjaxHooks.js
+++ src/components/AjaxHooks.js
@@ -12,8 +12,22 @@ function Pokemon({avatar, name}) {
 export default function AjaxHooks() {
     const [pokemons, setPokemons] = useState([]);
 useEffect(()=> {
+    
     const getPokemons = async (url) => {
-        let res = await fetch(el.url),
-    }
-})
+        let res = await fetch(url),
+        json = await res.json();
+
+        json.result.forEach(async (el) => {
+            let res = await fetch(el.url),
+            json = await res.json();
+
+        let Pokemon = {
+            id: json.id,
+            name: json.name,
+            avatar: json.sprites.front_default,
+        };
+        
+        }
+    };
+});
 }
