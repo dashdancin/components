6e8a3c68b4246acacb06630078edc9227d778134
dashdancing <omar.gbet@gmail.com>
2022-05-07 12:23:14 -0500

    css modifyed

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import AjaxApis from './components/AjaxApis';
 import ContadorHooks from './components/ContadorHooks';
 import ScrollHooks from './components/ScrollHooks';
 import RelojHooks from './components/RelojHooks';
-import './App.css';
 import AjaxHooks from './components/AjaxHooks';
 import HooksPersonalizados from './components/HooksPersonalizados';
 import Referencias from './components/Referencias';
 import Formularios from './components/Formularios';
+import './App.css';
 
 function App() {
   return (
