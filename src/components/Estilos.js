132edceaa6991ed0ed47bd06925839bb492c5aaa
dashdancing <omar.gbet@gmail.com>
2022-05-07 12:34:59 -0500

    ADD css Archive styles h3

---
+++ src/components/Estilos.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Estilos.css';
+
+export default function Estilos () {
+    // let myStyles = {
+    //     borderRadius: '.5rem',
+    //     margin: '2rem auto',
+    //     maxWidth: '50%'
+    // };
+
+    return (
+        <section className='estilos'>
+            <h2>Estilos CSS en React</h2>
+            <h3 className='bg-react'>Estilos en hoja CSS externa</h3>
+            <h3>
+                a
+            </h3>
+        </section>
+    )
+}
+
