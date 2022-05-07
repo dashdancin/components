1a827232cefe7518cf0e4c81916d09451e5d784f
dashdancing <omar.gbet@gmail.com>
2022-05-06 19:40:21 -0500

    ADD Formularios

---
+++ src/components/Fromularios.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export default function Formularios() {
+    const [nombre, setNombre] =useState ("");
+    const [sabor, setSabor] = useState ("");
+    const [lenguaje, SetLenguaje] = useState ("");
+    const [terminos, setTerminos] = useState (false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert('El formulario se ha enviado');
+    };
+
+    return (
+        <>
+            <h2>Formularios</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='nombre'>Nombre</label>
+            </form>
+        </>
+    )
+}
